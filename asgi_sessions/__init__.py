4873947eb116861286c6ba1aaf58ef3a57859a88
Kirill Klenov <horneds@gmail.com>
2021-12-14 11:10:57 +0300

    refactor: formatting

--- asgi_sessions/__init__.py
+++ asgi_sessions/__init__.py
@@ -4,14 +4,13 @@ from __future__ import annotations
 
 import sys
 import typing as t
-from base64 import urlsafe_b64encode, urlsafe_b64decode
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 from http import cookies
 
 from asgi_tools import Request, Response
-from asgi_tools.middleware import BaseMiddeware, ASGIApp
-from asgi_tools.typing import JSONType, Scope, Receive, Send
 from asgi_tools._compat import json_dumps, json_loads
-
+from asgi_tools.middleware import ASGIApp, BaseMiddeware
+from asgi_tools.typing import JSONType, Receive, Scope, Send
 
 try:
     from cryptography.fernet import Fernet, InvalidToken
@@ -29,16 +28,22 @@ __version__ = "1.1.1"
 __license__ = "MIT"
 
 
-__all__ = 'SessionMiddleware', 'Session', 'SessionJWT', 'SessionFernet'
+__all__ = "SessionMiddleware", "Session", "SessionJWT", "SessionFernet"
 
 
 class SessionMiddleware(BaseMiddeware):
     """Support sessions."""
 
     def __init__(
-            self, app: ASGIApp, secret_key: str = None, session_type: str = 'base64',
-            cookie_name: str = 'session', max_age: int = 14 * 24 * 3600, samesite: str = 'lax',
-            secure: bool = False):
+        self,
+        app: ASGIApp,
+        secret_key: str = None,
+        session_type: str = "base64",
+        cookie_name: str = "session",
+        max_age: int = 14 * 24 * 3600,
+        samesite: str = "lax",
+        secure: bool = False,
+    ):
         """Init the middleware."""
         super(SessionMiddleware, self).__init__(app)
         assert secret_key, "secret_key is required"
@@ -46,48 +51,54 @@ class SessionMiddleware(BaseMiddeware):
         self.cookie_name = cookie_name
         self.session_type = session_type
 
-        self.cookie_params: t.Dict[str, t.Any] = {'path': '/'}
+        self.cookie_params: t.Dict[str, t.Any] = {"path": "/"}
         if max_age:
-            self.cookie_params['max-age'] = max_age
+            self.cookie_params["max-age"] = max_age
         if secure:
-            self.cookie_params['secure'] = secure
+            self.cookie_params["secure"] = secure
         if sys.version_info >= (3, 8) and samesite:  # XXX: Python 3.7
-            self.cookie_params['samesite'] = samesite
+            self.cookie_params["samesite"] = samesite
 
-    async def __process__(self, scope: t.Union[Scope, Request], receive: Receive, send: Send):
+    async def __process__(
+        self, scope: t.Union[Scope, Request], receive: Receive, send: Send
+    ):
         """Load/save the sessions."""
         # Support asgi_tools.RequestMiddleware
         if isinstance(scope, Request):
             request = scope
         else:
-            request = scope.get('request') or Request(scope)
+            request = scope.get("request") or Request(scope)
 
         session = self.init_session(request.cookies.get(self.cookie_name))
-        scope['session'] = session
+        scope["session"] = session
 
         # Common ASGI Applications
         def send_wrapper(message):
             """Inject sessions cookie."""
             if session.modified and message["type"] == "http.response.start":
-                message['headers'].append((
-                    b'Set-Cookie',
-                    session.cookie(self.cookie_name, self.cookie_params).encode(),
-                ))
+                message["headers"].append(
+                    (
+                        b"Set-Cookie",
+                        session.cookie(self.cookie_name, self.cookie_params).encode(),
+                    )
+                )
 
             return send(message)
 
         # Support ASGI-Tools Responses
         response = await self.app(scope, receive, send_wrapper)
         if response and isinstance(response, Response) and session.modified:
-            response.headers['Set-Cookie'] = session.cookie(self.cookie_name, self.cookie_params)
+            response.headers["Set-Cookie"] = session.cookie(
+                self.cookie_name, self.cookie_params
+            )
 
         return response
 
     def init_session(self, token: str = None) -> Session:
-        if self.session_type == 'jwt':
+        if self.session_type == "jwt":
             return SessionJWT(token, secret=self.secret_key)
 
-        if self.session_type == 'fernet':
+        if self.session_type == "fernet":
             return SessionFernet(token, secret=self.secret_key)
 
         return Session(token)
@@ -156,17 +167,17 @@ class SessionJWT(Session):
 
     def __init__(self, *args, secret=None, **kwargs):
         if jwt is None:
-            raise RuntimeError('Install jwt package to use JWT sessions.')
+            raise RuntimeError("Install jwt package to use JWT sessions.")
 
         if not secret:
-            raise ValueError('SessionJWT.secret is required.')
+            raise ValueError("SessionJWT.secret is required.")
 
         self.secret = secret
         super(SessionJWT, self).__init__(*args, **kwargs)
 
     def encode(self) -> str:
         """Encode the session's data."""
-        token = jwt.encode(self, key=self.secret, algorithm='HS256')
+        token = jwt.encode(self, key=self.secret, algorithm="HS256")
         # Support JWT<2 (Remove me after 2022-01-01)
         if isinstance(token, bytes):
             token = token.decode()
@@ -174,7 +185,7 @@ class SessionJWT(Session):
 
     def decode(self, token, silent=True) -> t.Dict:
         try:
-            payload = jwt.decode(token, key=self.secret, algorithms=['HS256'])
+            payload = jwt.decode(token, key=self.secret, algorithms=["HS256"])
             return t.cast(t.Dict, payload)
         except jwt.DecodeError:
             if not silent:
@@ -188,14 +199,14 @@ class SessionFernet(Session):
 
     def __init__(self, *args, secret=None, **kwargs):
         if Fernet is None:
-            raise RuntimeError('Install cryptography package to use fernet sessions.')
+            raise RuntimeError("Install cryptography package to use fernet sessions.")
 
         if not secret:
-            raise ValueError('SessionFernet.secret is required.')
+            raise ValueError("SessionFernet.secret is required.")
 
         if len(secret) != 32:
             secret = secret[:32]
-            secret += '=' * (32 - len(secret) % 32)
+            secret += "=" * (32 - len(secret) % 32)
 
         self.secret = urlsafe_b64encode(secret.encode())
         self.f = Fernet(self.secret)
